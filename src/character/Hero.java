3d37956fb7dfa26031e3cc868388ec144ceb9c02
hiroki-labmac <hiroki2101@gmail.com>
2015-06-18 16:55:35 +0900

    RPG practice

---
+++ src/character/Hero.java
@@ -0,0 +1,36 @@
+package character;
+
+public class Hero {
+
+	//フィールド名は最初以外の単語の頭が大文字であると良い
+	String name;	//名前の宣言
+
+	int hp;	//HPの宣言
+
+	void attack(){
+	}
+	void run(){
+		System.out.println(this.name+"は逃げ出した！");
+		System.out.println("game over");
+		System.out.println("最終HPは"+this.hp+"でした");
+
+	}
+	void sit(int sec){
+		this.hp += sec;
+		System.out.println(this.name +"は" +sec+"秒すわった");
+		System.out.println("HPが" +sec+"ポイント回復した");
+
+	}
+	void slip(){
+
+
+	}
+	//メソッド名は最初以外の単語の頭が大文字であると良い
+	void sleep(){
+		this.hp = 100;	//thisは特別な変数で「自分自身のインスタンス」を意味する
+		System.out.println(this.name + "は、眠って回復した");
+	}
+
+}
+
+
---
+++ src/character/Matango.java
@@ -0,0 +1,9 @@
+package character;
+
+public class Matango {
+
+	int hp;
+	final int level = 10;	//finalを変数の前に付けると定数として宣言　フィールドLEVELは10で固定
+
+
+}
