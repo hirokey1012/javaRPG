b535b5d74755a5332e9cdc0a1caee4937ea0d8c6
hiroki-labmac <hiroki2101@gmail.com>
2015-06-25 16:45:58 +0900

    complete chapter-9 from labmac

--- src/character/Cleric.java
+++ src/character/Cleric.java
@@ -6,19 +6,19 @@ import java.util.*;
 public class Cleric {
 
 	String name;
-	int hp = 50;
-	final int max_hp = 50;
-	int mp = 10;
-	final int max_mp = 10;
+	int hp;
+	int mp;
+	static final int max_hp = 50;
+	static final int max_mp = 10;
 
-	public void selfAid(){
+	void selfAid(){
 		System.out.println(this.name + "はセリフエイドを唱えた");
-		this.hp = this.max_hp;
+		this.hp = Cleric.max_hp;
 		this.mp -= 5;
 		System.out.println("HPが最大まで回復した");
 	}
 
-	public int pray(int sec){
+	int pray(int sec){
 
 		System.out.println(this.name + "は" + sec + "秒間天に祈りを捧げた");
 
@@ -26,12 +26,28 @@ public class Cleric {
 		int recover = new Random().nextInt(3) + sec;
 
 		//実際の回復量を計算する
-		//上限MP - 現在MP　と　回復量を比較して　小さい方がrecoverActualに入る（上限MPを超える回復量はありえない）
-		int recoverActual = Math.min(this.max_mp -this.mp, recover);
+		//上限MP - 現在MP　と　回復量を比較して　小さい方がrecoverActualに入る（現在MP＋回復量が上限MPを突破してしまう恐れ）
+		int recoverActual = Math.min(Cleric.max_mp - this.mp, recover);
 		this.mp += recoverActual;
 		System.out.println("MPが"+recoverActual + "回復した");
 
 		return recoverActual;
 	}
+
+	//コンストラクタはクラス名(引数){ 初期化の処理 }
+	Cleric(String name, int hp, int mp){
+		this.name = name;
+		this.hp = hp;
+		this.mp = mp;
+	}
+
+	Cleric(String name, int hp){
+		this(name, hp, Cleric.max_mp);
+	}
+
+	Cleric(String name){
+		this(name , Cleric.max_hp);
+	}
+
 
 }
--- src/character/Main.java
+++ src/character/Main.java
@@ -1,16 +1,30 @@
 package character;
 
 public class Main {
+	//mainメソッドは常にstatic（mainメソッド呼び出し時にはインスタンスは存在していないから）
 	public static void main(String[] args){
 
 		//勇者を生成
-		Hero h = new Hero();
-
+		//Hero型のh1には、生み出されたHeroインスタンスの番地が入っているだけ（実体はない）
+		Hero h1 = new Hero("ミナト",500);		//コンストラクタ1
 		//フィールドに初期値をセット
-		h.name = "ミナト";
-		h.hp = 100;
-		System.out.println("勇者" + h.name + "を生み出しました");
+		System.out.println("勇者" + h1.name + "を生み出しました。彼のHPは" + h1.hp + "です");
 
+		//勇者を生成
+		Hero h2 = new Hero();		//コンストラクタ2（引数がないとき）
+		System.out.println("勇者" + h2.name + "を生み出しました。彼のHPは" + h2.hp + "です");
+
+		//ランダムな金額を設定（しなければ初期値の¥1000）
+		Hero.setRandomMoney();
+
+		//moneyはHeroクラスの静的フィールド（クラス変数）
+		System.out.println("勇者らの共通貯金は¥" + Hero.money + "です");
+
+		//静的フィールドにはたとえインスタンスが生成されていない状態でもアクセスできる
+
+		//h1.moneyの値を変えると、h2.moneyの値も変化する（どうせならHero.moneyを使った方が良い）
+		//h1.money += 500;
+		//System.out.println("勇者らの共通貯金は¥" + h2.money + "です");
 
 		//きのこを生成
 		Matango m1 = new Matango();
@@ -21,13 +35,7 @@ public class Main {
 		m2.hp = 48;
 		m2.suffix = 'B';
 
-
 		//冒険の始まり
-		h.slip();
-		m1.run();
-		m2.run();
-		h.run();
-
 
 	}
 
